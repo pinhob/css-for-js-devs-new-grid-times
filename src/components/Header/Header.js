08203cdcb2de2d96b34f730734af09aa3f61312d
bruno pinho <brunopinho@outlook.com>
2023-04-08 17:14:12 -0300

    feat (Header.js): exercise 1 - header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,23 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopNav>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopNav>
+
         <Logo />
+
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <StyledLink href="#">Already a subscriber?</StyledLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +55,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +85,48 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    justify-content: space-between;
+    margin-top: 16px;
+    margin-bottom: 81px;
+  }
+`;
+
+const DesktopNav = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+  position: relative;
+
+@media ${QUERIES.laptopAndUp} {
+  display: revert;
+  justify-self: end;
+}
+`;
+
+const StyledLink = styled.a`
+  margin-top: 8px;
+  text-decoration: underline;
+  font-weight: 400;
+  font-size: ${14 / 16}rem;
+  font-style: italic;
+  text-align: center;
+  width: 100%;
+  position: absolute;
 `;
 
 export default Header;
