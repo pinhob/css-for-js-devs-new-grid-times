9f4dd54242d6bb1005b76db758df65964f7c7d57
bruno pinho <brunopinho@outlook.com>
2023-04-13 20:11:30 -0300

    feat(SpecialtyStoryGrid.js): ecercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -6,6 +6,7 @@ import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
+import { QUERIES } from '../../constants';
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -47,14 +48,43 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    border-right: 1px solid var(--color-gray-300);
+    margin-right: 24px;
+    padding-right: 24px;
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+    display: grid;
+    gap: 16px;
+    grid-template-columns:
+      repeat(auto-fill, minmax(150px, 1fr));
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+    display: grid;
+    gap: 16px;
+    overflow: auto;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+
+    
+    @media ${QUERIES.tabletAndUp} {
+      grid-auto-flow: column;
+      grid-template-columns: repeat(auto-fill, 220px);
+    
+    }
+    
+`;
 
 export default SpecialtyStoryGrid;
