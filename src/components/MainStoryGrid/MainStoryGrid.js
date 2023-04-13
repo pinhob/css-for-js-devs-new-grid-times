fa4d1bf480a4877153f224ce56fce7852e04c69b
bruno pinho <brunopinho@outlook.com>
2023-04-13 03:11:48 -0300

    feat(MainStory.js and SecondaryStory.js): exercise 3 - main story grid

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -12,6 +12,7 @@ import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
+import { QUERIES } from '../../constants';
 
 const MainStoryGrid = () => {
   return (
@@ -32,19 +33,19 @@ const MainStoryGrid = () => {
 
       <OpinionSection>
         <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
+        <OpinionListTabletWrapper>
           {OPINION_STORIES.map((story, index) => (
-            <VerticalStoriesWrapper>
+            <OpinionStoriesWrapper>
               <OpinionStory key={story.id} {...story} />
-            </VerticalStoriesWrapper>
+            </OpinionStoriesWrapper>
           ))}
-        </StoryList>
+        </OpinionListTabletWrapper>
       </OpinionSection >
 
       <AdvertisementSection>
         <Advertisement />
       </AdvertisementSection>
-    </Wrapper >
+    </Wrapper>
   );
 };
 
@@ -57,14 +58,42 @@ const Wrapper = styled.div`
     'advertisement';
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-areas:
+    'main-story secondary-stories'
+    'advertisement advertisement'
+    'opinion-stories opinion-stories';
+    grid-template-columns: 2fr 1fr;
+    gap: 48px 0;
+  };
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas:
+    'main-story secondary-stories opinion-stories'
+    'main-story advertisement advertisement';
+    grid-template-columns: 5fr 4fr 3fr;
+    gap: 0;
+  };
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+  @media ${QUERIES.tabletAndUp} {
+    padding-right: 16px;
+  margin-right: 16px;
+  border-right: 1px solid var(--color-gray-300);
+  } 
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+  margin-right: 16px;
+  border-right: 1px solid var(--color-gray-300);
+  } 
 `;
 
 const StoryList = styled.div`
@@ -73,18 +102,55 @@ const StoryList = styled.div`
 `;
 
 const VerticalStoriesWrapper = styled.div`
-padding: 16px 0;
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-gray-300);
+    padding-bottom: 16px;
+    margin-bottom: 16px;
   };
+
+  @media ${QUERIES.tabletOnly} {
+    &:not(:last-child) {
+      border-bottom: none;
+      padding-bottom: 0; 
+    };
+  } 
 `;
 
 const OpinionSection = styled.section`
   grid-area: opinion-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: -8px;
+  }
+`;
+
+const OpinionListTabletWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media ${QUERIES.tabletOnly} {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 32px;
+  }
+`;
+
+const OpinionStoriesWrapper = styled(VerticalStoriesWrapper)`
+  @media ${QUERIES.tabletOnly} {
+      &:not(:last-child) {
+      border-bottom: none;
+    };
+    }
 `;
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-top: 24px;
+  margin-top: 24px;
+  border-top: 1px solid var(--color-gray-300);
+  } 
 `;
 
 export default MainStoryGrid;
--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 const SecondaryStory = ({ id, title, image, location, abstract }) => {
   return (
@@ -21,7 +22,14 @@ const Wrapper = styled.article`
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
-  /* padding: 16px 0; */
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-areas:
+      'image'	
+      'heading'
+      'abstract';
+      grid-template-columns: 1fr;
+  }
 `;
 
 const Image = styled.img`
